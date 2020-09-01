af38b8adae3c596f0140fa458c094b692e2a7903
Yuriy <ddearcj@gmail.com>
2020-09-01 14:18:34 +0300

    fixed cors

--- src/whoami/main.go
+++ src/whoami/main.go
@@ -114,7 +114,6 @@ func addCorsHeader(res http.ResponseWriter) {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 	//	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
